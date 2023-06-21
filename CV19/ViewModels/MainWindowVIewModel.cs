500de25a8c7efaec29a3a73151d494f5d651a1ef
ss_rodin <ss_rodin@MT25-N-00165>
2023-06-21 17:08:26 +1000

    1

---
+++ CV19/ViewModels/MainWindowVIewModel.cs
@@ -0,0 +1,29 @@
+﻿using CV19.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV19.ViewModels
+{
+    internal class MainWindowVIewModel : ViewModel
+    {
+        #region ЗаголовокОкна
+        private string _Title = "Анализ статистики CV19";
+        
+        ///<summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _Title;
+            set
+            {
+                //if (Equals(_Title, value)) return;
+                //_Title = value;
+                //OnProperyChanged();
+                Set(ref _Title, value);
+            }
+        } 
+        #endregion
+    }
+}
