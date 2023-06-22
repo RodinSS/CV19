01b5de6ad03a2aaf0c2df8cceb36f8f3c48661cf
ss_rodin <ss_rodin@MT25-N-00165>
2023-06-22 13:58:08 +1000

    Добавляем команды и кнопку выход

--- CV19/ViewModels/MainWindowVIewModel.cs
+++ CV19/ViewModels/MainWindowVIewModel.cs
@@ -1,9 +1,12 @@
-﻿using CV19.ViewModels.Base;
+﻿using CV19.Infrastructure.Commands;
+using CV19.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace CV19.ViewModels
 {
@@ -15,15 +18,44 @@ namespace CV19.ViewModels
         ///<summary>Заголовок окна</summary>
         public string Title
         {
-            get => _Title;
-            set
-            {
-                //if (Equals(_Title, value)) return;
-                //_Title = value;
-                //OnProperyChanged();
-                Set(ref _Title, value);
-            }
+            get => _Title; set => Set(ref _Title, value);
+            //{
+            //    //if (Equals(_Title, value)) return;
+            //    //_Title = value;
+            //    //OnProperyChanged();
+            //    Set(ref _Title, value);
+            //}
+        }
+        #endregion
+        #region СтатусПрограммы
+        private string _Status = "Готово";
+
+        ///<summary>СтатусПрограммы</summary>
+        public string Status
+        {
+            get => _Status; set => Set(ref _Status, value);
+        }
+        #endregion
+
+        #region Команды
+        #region CloseApplicationCommand
+        public ICommand CloseApplicationCommand { get; }
+
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+
+        private void OnCloseApplicationCommandExecuted(object p) 
+        { 
+            Application.Current.Shutdown();
         } 
         #endregion
+        #endregion
+        public MainWindowVIewModel()
+        {
+            #region Команды
+
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+
+            #endregion
+        }
     }
 }
