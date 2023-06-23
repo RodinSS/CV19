2a1f1cb96fad5f2e28ff17d765fb861bad2023d9
ss_rodin <ss_rodin@MT25-N-00165>
2023-06-23 12:29:32 +1000

    Парс 1

--- Tests/CV19ConsoleTest/Program.cs
+++ Tests/CV19ConsoleTest/Program.cs
@@ -30,7 +30,7 @@ namespace CV19ConsoleTest
             {
                 var line = data_reader.ReadLine();
                 if (string.IsNullOrWhiteSpace(line)) continue;
-                yield return line;
+                yield return line.Replace("Korea,", "Korea -");
             }
         }
 
@@ -40,6 +40,20 @@ namespace CV19ConsoleTest
             .Skip(4)
             .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
             .ToArray();
+
+        private static IEnumerable<(string Country, string Province, int[] Counts)> GetData()
+        {
+            var lines = GetDataLines()
+                .Skip(1)
+                .Select(line => line.Split(","));
+
+            foreach (var row in lines)
+            {
+                var province = row[0].Trim();
+                var country_name = row[1].Trim(' ', '"');
+                var counts = row.Skip(4).Select(int.Parse).ToArray();
+            }
+        }
 
         static void Main(string[] args)
         {
